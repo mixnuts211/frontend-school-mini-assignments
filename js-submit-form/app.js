120bd3c7522cbf91a676b239824358c4008f0dee
mixnuts211 <migyeong0211@gmail.com>
2022-10-25 00:38:50 +0900

    변수선언

---
+++ js-submit-form/app.js
@@ -0,0 +1,5 @@
+const mainForm = document.querySelector('#mainForm');
+const progressBar = mainForm.querySelector('.progress-bar');
+const notice = mainForm.querySelector('.notice');
+const inputs = mainForm.querySelectorAll('input');
+const resetBtn = mainForm.querySelector('button [type="reset]');
